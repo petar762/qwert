458417d882478c019f0f7a6ccafac6c4c87acca3
User <levomado123@gmail.com>
2022-07-05 12:49:09 +0300

    init

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner( System.in );
+        System.out.print( "Enter amount in dollars: " );
+        double dollar = myInput.nextDouble();
+        double lev = dollar * 1.65;
+
+        System.out.println("Dollars in bgn: " + lev + " bgn");
+
+
+    }
+
 }
